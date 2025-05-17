528a39c548c201cc845f2bed9b5cd3ea74a68fb2
Shiroya Fenil <shiroya.fenil@valtech.com>
2025-05-17 13:24:08 +0530

    Add-Book component and modification done on Admin-Dashboard with backend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,5 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+export const appConfig=[ provideRouter(routes)];
+ 
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin-dashboard/admin.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'add-book', component: AddBookComponent },
+  { path: '', redirectTo: '/admin', pathMatch: 'full' }
+];
+
+
+ 
